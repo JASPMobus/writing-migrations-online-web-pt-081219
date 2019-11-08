2f68ac39f8a51a14f46a1b20968c020c6d9ef4ef
James Mobus <mobusj@live.com>
2019-11-08 17:41:06 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,9 +2,8 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
+      t.string :grade
+      t.string :birthdate
     end
   end
 end
